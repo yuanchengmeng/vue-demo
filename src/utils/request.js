de78592edfb99a0c2d43867bbd861861e616aa82
yuanchengmeng <yuanchengmeng@jinjiedao.com>
2020-08-15 19:00:51 +0800

    1.0版本

---
+++ src/utils/request.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const service = axios.create({
+    baseURL: "/api",
+    timeout: 30000,
+    withCredentials: true
+});
+
+service.interceptors.request.use(
+    config => {
+        return config;
+    },
+    error => {
+        console.log(error);
+        return Promise.reject();
+    }
+);
+
+service.interceptors.response.use(
+    response => {
+        console.log(response);
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            Promise.reject();
+        }
+    },
+    error => {
+        console.log(error);
+        return Promise.reject();
+    }
+);
+
+export default service;
